320db00f6f62390fd77f7d563a98d23e51791c2f
solderq35 <solderq35@gmail.com>
2022-12-12 01:22:23 -0800

    favicon and metadata

--- fullgame.js
+++ fullgame.js
@@ -1,17 +1,17 @@
-var storedText;
+let storedText;
 
 //https://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
 if (!String.linkify) {
   String.prototype.linkify = function () {
     // http://, https://, ftp://
-    var urlPattern =
+    let urlPattern =
       /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
     // www. sans http:// or https://
-    var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    let pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
     // Email addresses
-    var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+    let emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
     return this.replace(urlPattern, '<a href="$&">$&</a>')
       .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
@@ -20,22 +20,22 @@ if (!String.linkify) {
 }
 
 function fetchInfo() {
-  var runLinkInput = document.getElementById("runLink").value;
-  var runDiffInput = document.getElementById("diffInput").value;
-  var runReasonInput = document.getElementById("reasonInput").value;
+  let runLinkInput = document.getElementById("runLink").value;
+  let runDiffInput = document.getElementById("diffInput").value;
+  let runReasonInput = document.getElementById("reasonInput").value;
   let idslice = runLinkInput.slice(-8);
-  let playerEmbed = "?embed=players,category.variables,level";
+  let playerEmbed = "?embed=players,category.letiables,level";
   let idPlusEmbeds = idslice.concat(playerEmbed);
   let apiDomain = "https://www.speedrun.com/api/v1/runs/";
   let apiUrl = apiDomain.concat(idPlusEmbeds);
-  var playerName = document.getElementById("playerCell");
-  var categoryName = document.getElementById("categoryCell");
-  var timeName = document.getElementById("timeCell");
-  var dateName = document.getElementById("dateCell");
-  var videoName = document.getElementById("videoCell");
-  var reasonName = document.getElementById("reasonCell");
-  var grunName = document.getElementById("grunCell");
-  var apiUrlName = document.getElementById("apiUrlDiv");
+  let playerName = document.getElementById("playerCell");
+  let categoryName = document.getElementById("categoryCell");
+  let timeName = document.getElementById("timeCell");
+  let dateName = document.getElementById("dateCell");
+  let videoName = document.getElementById("videoCell");
+  let reasonName = document.getElementById("reasonCell");
+  let grunName = document.getElementById("grunCell");
+  let apiUrlName = document.getElementById("apiUrlDiv");
 
   fetch(apiUrl).then(function (response) {
     response.text().then(function (text) {
@@ -43,12 +43,12 @@ function fetchInfo() {
       const obj = JSON.parse(text);
       //playerName.innerHTML = obj.data.players.data[0].names.international;
 
-      var finaltime;
-      var initialTime = obj.data.times.primary_t;
-      var finalTime;
-      var minutes;
-      var seconds;
-      var hours;
+      let finaltime;
+      let initialTime = obj.data.times.primary_t;
+      let finalTime;
+      let minutes;
+      let seconds;
+      let hours;
       //console.log(initialTime);
 
       if (obj.data.players.data[0].rel == "user") {
@@ -98,8 +98,8 @@ function fetchInfo() {
       videoName.innerHTML = obj.data.videos.links[0].uri.linkify();
       reasonName.innerHTML = runReasonInput;
       categoryName.innerHTML = obj.data.category.data.name + " " + runDiffInput;
-      var commentInitial = obj.data.comment;
-      var linkextract = commentInitial.match(/\bhttps?:\/\/\S+/gi);
+      let commentInitial = obj.data.comment;
+      let linkextract = commentInitial.match(/\bhttps?:\/\/\S+/gi);
       grunName.innerHTML = linkextract.toString().linkify();
       apiUrlName.innerHTML = apiUrl.linkify();
     });
